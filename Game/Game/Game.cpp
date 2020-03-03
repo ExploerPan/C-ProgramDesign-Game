e2fb25b8d3dfaf370ceeb2b281f642a62a713c91
pan <Vamoose@LAPTOP-SI20L0H5>
2020-03-03 14:43:10 +0800

    test2

---
+++ Game/Game/Game.cpp
@@ -0,0 +1,64 @@
+﻿// Game.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include<stdio.h>
+#include<easyx.h>
+#include<conio.h>
+#define WIDTH 1400
+#define HEIGHT 800
+
+typedef struct {
+	char name[20];
+	int score;
+}USER;
+
+void init(USER& user);
+void initback();
+
+int main() {
+	USER user;
+	user.score = 0;
+	init(user);
+	initback();
+	return 0;
+}
+
+void initback() {
+	IMAGE back;
+	initgraph(WIDTH, HEIGHT, SHOWCONSOLE);
+	loadimage(NULL, "image\\background.jpg", WIDTH, HEIGHT);
+	setlinecolor(BLACK);
+	line(1100, 0, 1100, 800);
+	_getch();
+}
+
+
+void init(USER& user)
+{
+	initgraph(WIDTH, HEIGHT);
+	loadimage(NULL, "image\\welcomebackground.jpg", WIDTH, HEIGHT);
+
+	InputBox(user.name, 20, "请输入用户名：");
+	outtextxy(WIDTH / 2 + 400, HEIGHT / 2 - 200, "Name: ");
+	outtextxy(WIDTH / 2 + 450, HEIGHT / 2 - 200, user.name);
+
+	char buf[10];
+	sprintf_s(buf, "%d", user.score);   //user.score为整型，outtextxy要求输出的是 char[]
+	outtextxy(WIDTH / 2 + 400, HEIGHT / 2 - 150, "Score: ");
+	outtextxy(WIDTH / 2 + 450, HEIGHT / 2 - 150, buf);
+}
+
+
+
+
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门使用技巧: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
