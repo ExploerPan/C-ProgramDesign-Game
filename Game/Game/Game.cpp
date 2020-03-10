3e31a966826b9e8e5fbe1fa7a0a2ceb3f6dc806d
pan <2523773317@qq.com>
2020-03-10 18:26:51 +0800

    制作了储存题目的链表

--- Game/Game/Game.cpp
+++ Game/Game/Game.cpp
@@ -2,10 +2,12 @@
 //
 
 #include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
 #include<easyx.h>
 #include<conio.h>
-#include<mmsystem.h>			
-#include<Windows.h>
+#include<mmsystem.h>	
+#include <tchar.h>
 #pragma comment(lib,"winmm.lib")
 #define WIDTH 1600
 #define HEIGHT 800
@@ -16,10 +18,23 @@ typedef struct {
 	int score;
 }USER;
 
+typedef struct Problem {
+	int number1;
+	int number2;
+	char operation;
+	int result;
+	struct Problem* next;
+}PROBLEM;
+
 void init(USER& user);
-void initback();                            //初始化背景
-void playMusic(int a,int b);                 //a.b表示音乐播放状态
-int judgeMessage(int x,int y);                //判断鼠标点击的按钮
+void initback();									 //初始化背景
+void initbomb();									//初始化炸弹图片
+void playMusic(int a,int b);						 //a.b表示音乐播放状态
+int judgeMessage(int x,int y);						//判断鼠标点击的按钮
+void newProblem(int* r,int* n1,int *n2,char *op);				//为每一个结点生成题目
+void playGame();
+
+
 
 int main() {
 	int message=0,x=0,y=0;
@@ -30,15 +45,21 @@ int main() {
 	MOUSEMSG m;
 	init(user);
 	initback();
+	initbomb();
 	while (1) {
 		m = GetMouseMsg();
 		if (m.uMsg == WM_LBUTTONDOWN) {
 			x = m.x;                                 //x为横坐标
 			y = m.y;									// y为纵坐标
-			message=judgeMessage(x,y);
+			message = judgeMessage(x, y);
+
 			switch (message) {
+			case 1:
+				playGame();
+				break;
+
 			case 6:
-				isExit = MessageBox(NULL,"确定要退出游戏吗？", "提示", MB_YESNO| MB_SYSTEMMODAL);
+				isExit = MessageBox(NULL, "确定要退出游戏吗？", "提示", MB_YESNO | MB_SYSTEMMODAL);
 				if (isExit == 6) {
 					exit(0);
 				}
@@ -57,8 +78,24 @@ int main() {
 	return 0;
 }
 
+void init(USER& user)
+{
+	initgraph(WIDTH, HEIGHT);
+	loadimage(NULL, "image\\welcomebackground.jpg", WIDTH, HEIGHT);
+
+	InputBox(user.name, 20, "请输入用户名：");
+	outtextxy(WIDTH / 2 + 400, HEIGHT / 2 - 200, "Name: ");
+	outtextxy(WIDTH / 2 + 450, HEIGHT / 2 - 200, user.name);
+
+	char buf[10];
+	sprintf_s(buf, "%d", user.score);   //user.score为整型，outtextxy要求输出的是 char[]
+	outtextxy(WIDTH / 2 + 400, HEIGHT / 2 - 150, "Score: ");
+	outtextxy(WIDTH / 2 + 450, HEIGHT / 2 - 150, buf);
+}
+
 void initback() {
 	IMAGE start,add,hint,exit,save,read,music,music_mask,button_mask;
+
 	initgraph(WIDTH, HEIGHT, SHOWCONSOLE);
 	loadimage(NULL, "image\\background.jpg", WIDTH, HEIGHT);
 	setlinecolor(BLACK);
@@ -93,10 +130,22 @@ void initback() {
 	loadimage(&music_mask, "image\\music_mask.jpg", 330, 80);
 	putimage(WIDTH - 330, HEIGHT / 2 + 300, &music_mask,SRCAND);
 	putimage(WIDTH - 330, HEIGHT / 2 + 300, &music,SRCINVERT);
+	
+	settextstyle(20, 20, "宋体");
+	setbkmode(TRANSPARENT);
+	outtextxy(WIDTH - 250, HEIGHT / 2 - 100, "当前题目");
 
 }
 
-
+void initbomb() {
+	IMAGE bomb, bomb_mask;
+	int x = WIDTH - 1000, y = 0;
+	loadimage(&bomb, "image\\bomb.jpg", 240, 160);
+	loadimage(&bomb_mask, "image\\bomb_mask.jpg", 240, 160);
+	putimage(x, y, &bomb_mask, SRCAND);
+	putimage(x, y, &bomb, SRCPAINT);
+	
+}
 
 int judgeMessage(int x, int y) {
 	int message;
@@ -139,22 +188,95 @@ void playMusic(int a,int b) {
 		mciSendString("resume music.mp3", 0, 0, 0);
 	}
 }
+
+PROBLEM *initProblem() {
+	int num = 1;
+	int num1 = 0, num2 = 0, res = 0;
+	char ope = '+';								
+	int* a = &res;								//存储表达式的结果
+	int* b = &num1;							//生成的第一个数字
+	int* c = &num2;							//生成的第二个数字
+	char* op = &ope;						////生成的运算符号
+	PROBLEM * p, * head = NULL, * r = NULL;
+	while (num < 15) {
+		p = (PROBLEM*)malloc(sizeof(PROBLEM));
+		newProblem(a,b,c,op);
+		if (p == NULL) {
+			printf("内存分配不成功！");
+		}
+		else {
+			p->number1 = num1;
+			p->number2 = num2;
+			p->operation = ope;
+			p->result = res;
+			p->next = NULL;
+			if (head == NULL) {
+				head = p;
+				r = p;
+			}
+			else {
+				r->next = p;
+				r = p;
+			}
+			num++;
+		}
+		return head;
+	}
+
+}
+
+void newProblem(int* r,int* n1,int* n2,char* op) {
+	int op_kind;
+	srand((int)time(NULL));
+	*n1 = rand() % 101;
+	*n2 = rand() % 101;
+	op_kind = rand() % 5;
+	switch (op_kind) {
+	case 1:
+		*op = '+';
+		break;
+	case 2:
+		*op = '-';
+		*r = *n1 - *n2;
+		break;
+	case 3:
+		*op = '*';
+		*r = *n1 * *n2;
+		break;
+	case 4:
+		*op = '/';
+		*r = *n1 / *n2;
+		break;
+	}
+	
+}
+
+
+
+void playGame() {
+	char s[5];							//声明数组，用来存放字符串 
+	PROBLEM* head;
+	head = initProblem();
+
+	while (head != NULL) {
+		_stprintf_s(s, "%d", head->number1);					//将整型转换成字符串
+		outtextxy(WIDTH - 300, HEIGHT / 2 - 50, s);
+
+		outtextxy(WIDTH - 240, HEIGHT / 2 - 50, head->operation);
+
+		_stprintf_s(s, "%d", head->number2);
+		outtextxy(WIDTH - 200, HEIGHT / 2 - 50, s);
+
+		outtextxy(WIDTH - 140, HEIGHT / 2 - 50, "=");
+
+		outtextxy(WIDTH - 80, HEIGHT / 2 - 50, "?");
+		
+		head = head->next;
+	}
+	
+}
 	
 
-void init(USER& user)
-{
-	initgraph(WIDTH, HEIGHT);
-	loadimage(NULL, "image\\welcomebackground.jpg", WIDTH, HEIGHT);
-
-	InputBox(user.name, 20, "请输入用户名：");
-	outtextxy(WIDTH / 2 + 400, HEIGHT / 2 - 200, "Name: ");
-	outtextxy(WIDTH / 2 + 450, HEIGHT / 2 - 200, user.name);
-
-	char buf[10];
-	sprintf_s(buf, "%d", user.score);   //user.score为整型，outtextxy要求输出的是 char[]
-	outtextxy(WIDTH / 2 + 400, HEIGHT / 2 - 150, "Score: ");
-	outtextxy(WIDTH / 2 + 450, HEIGHT / 2 - 150, buf);
-}
 
 
 
